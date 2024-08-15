b18927b6affa961550124ed606238317d0a4ba55
Technology Rooms <technologyrooms@colsanjose.edu.co>
2024-08-15 14:38:22 -0500

    mi primer programtia en java

---
+++ main.java
@@ -0,0 +1,13 @@
+
+class Main {
+    public static void main(String args[]){
+
+        int x = 0;
+        float y = 0.5f;
+        double z = 1000; 
+
+        System.out.println("Hello world!");
+
+    }
+
+}
